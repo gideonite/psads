57ac4f3f15559630fd979144f69af54a36c1ed43
Gideon Dresdner <gideond@gmail.com>
2014-01-08 16:53:19 -0500

    Stacks and queues.

---
+++ ch4-queues/printer-simulation.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+from queue import Queue
+
+import random
+
+class Printer:
+    def __init__(self, ppm):
+        self.pagerate = ppm
+        self.currentTask = None
+        self.timeRemaining = 0
+
+    def tick(self):
+        if self.currentTask != None:
+            self.timeRemaining = self.timeRemaining - 1
+            if self.timeRemaining <= 0:
+                self.currentTask = None
+
+    def busy(self):
+        if self.currentTask != None:
+            return True
+        else:
+            return False
+
+    def startNext(self,newtask):
+        self.currentTask = newtask
+        self.timeRemaining = newtask.getPages() * 60/self.pagerate
+
+class Task:
+    def __init__(self,time):
+        self.timestamp = time
+        self.pages = random.randrange(1,21)
+
+    def getStamp(self):
+        return self.timestamp
+
+    def getPages(self):
+        return self.pages
+
+    def waitTime(self, currenttime):
+        return currenttime - self.timestamp
+
+
+def simulation(numSeconds, numStudents, pagesPerMinute):
+
+    labprinter = Printer(pagesPerMinute)
+    printQueue = Queue()
+    waitingtimes = []
+
+    for currentSecond in range(numSeconds):
+
+      if newPrintTask(numStudents):
+         task = Task(currentSecond)
+         printQueue.enqueue(task)
+
+      if (not labprinter.busy()) and (not printQueue.isEmpty()):
+        nexttask = printQueue.dequeue()
+        waitingtimes.append( nexttask.waitTime(currentSecond))
+        labprinter.startNext(nexttask)
+
+      labprinter.tick()
+
+    averageWait=sum(waitingtimes)/len(waitingtimes)
+    print("Average Wait %6.2f secs %3d tasks remaining."%(averageWait,printQueue.size()))
+
+def newPrintTask(numStudents):
+
+    avg_no_print_jobs_per_hour_per_student = 2
+    avg_no_tasks_per_hour = numStudents * avg_no_print_jobs_per_hour_per_student
+    avg_no_tasks_per_second = avg_no_tasks_per_hour / 3600.0
+
+    num = random.randrange(1, int(1 // avg_no_tasks_per_second))
+
+    if num == 1:
+        return True
+    else:
+        return False
+
+    if rando > avg_no_tasks_per_second * 0.5:
+        return True
+    else:
+        return False
+
+for i in range(10):
+    simulation(3600,10,5)
