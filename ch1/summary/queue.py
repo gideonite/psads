0b0243b027806f4aa9a57255087178c267f1c471
Gideon Dresdner <gideond@gmail.com>
2014-01-16 14:46:13 -0500

    Added size to fast queue impl.

--- ch1/summary/queue.py
+++ ch1/summary/queue.py
@@ -4,17 +4,21 @@ class Queue:
     '''
     A queue that supports, on average, constant time enqueue and dequeue.
     '''
+
     def __init__(self):
         self.enqueue_stack = []
         self.dequeue_stack = []
+        self.count = 0
 
     def isEmpty(self):
         return [] == self.enqueue_stack and [] == self.dequeue_stack
 
     def enqueue(self, item):
+        self.count += 1
         self.enqueue_stack.append(item)
 
     def dequeue(self):
+        self.count -= 1
         if [] == self.dequeue_stack:
 
             while [] != self.enqueue_stack:
@@ -24,10 +28,14 @@ class Queue:
 
         return self.dequeue_stack.pop()
 
+    def size(self):
+        return self.count
+
 q = Queue()
 q.enqueue(1)
 q.enqueue(2)
 q.enqueue(3)
+assert 3 == q.size()
 assert 1 == q.dequeue()
 assert 2 == q.dequeue()
 assert 3 == q.dequeue()
