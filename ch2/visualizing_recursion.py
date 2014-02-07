d144be6f3ec4b9b99c8736685d06a35078318574
Gideon Dresdner <gideond@gmail.com>
2014-02-07 16:22:54 -0500

    Sierpinksi Triangle

--- ch2/visualizing_recursion.py
+++ ch2/visualizing_recursion.py
@@ -39,3 +39,46 @@ def run_tree():
     myScreen.exitonclick()
 
 # run_tree()
+
+def draw_triangle(points, color, turtle):
+    turtle.fillcolor(color)
+    turtle.up()
+    turtle.goto(points[0][0], points[0][1])
+    turtle.down()
+
+    turtle.begin_fill()
+    turtle.goto(points[1][0], points[1][1])
+    turtle.goto(points[2][0], points[2][1])
+    turtle.goto(points[0][0], points[0][1])
+    turtle.end_fill()
+
+def midpoint(p,q):
+    return ( (p[0]+q[0]) / 2, (p[1]+q[1]) / 2)
+
+def siepinksi(points, degree, turtle):
+    colors = ['blue','red','green','white','yellow','violet','orange']
+
+    draw_triangle(points, colors[degree], turtle)
+
+    if degree > 0:
+        siepinksi([points[0],\
+                    midpoint(points[0], points[1]),\
+                    midpoint(points[0], points[2])],\
+                degree-1, turtle)
+        siepinksi([points[1],\
+                    midpoint(points[0], points[1]),\
+                    midpoint(points[1], points[2])],\
+                degree-1, turtle)
+        siepinksi([points[2],\
+                    midpoint(points[2], points[1]),\
+                    midpoint(points[0], points[2])],\
+                degree-1, turtle)
+
+def run_siepinski():
+    myTurtle = turtle.Turtle()
+    myScreen = turtle.Screen()
+    points = [[-100,-50], [0,100], [100,-50]]
+    siepinksi(points, 3, myTurtle)
+    myScreen.exitonclick()
+
+run_siepinski()
