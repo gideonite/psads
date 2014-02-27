0905c6eeac64e463cb9fe711111db33b15bf0533
Gideon Dresdner <gideond@gmail.com>
2014-02-27 11:48:03 -0500

    A bunch of recursion exercises.
    TODO: edit distance with dynamic programming instead of recursion.

--- ch2/summary/exercises.py
+++ ch2/summary/exercises.py
@@ -37,9 +37,9 @@ def fib_gen():
         yield a
         a,b = b,a+b
 
-f = fib_gen()
-f.next()
-print [f.next() for i in range(100)]
+# f = fib_gen()
+# f.next()
+# print [f.next() for i in range(100)]
 
 def fibonacci2(num_terms):
     a,b = 0,1
@@ -87,7 +87,137 @@ def hanoi(num_disks):
         flip_index += 1
         flip_index %= 3
 
+# hanoi(10)
 
-hanoi(10)
+import turtle
 
-# hanoi(range(4), [], [])
+def hilbert(turtle, state, order, side_length):
+    '''
+    Lindenmayer System Rules (wikipedia.org/wiki/Hilbert_curve)
+    Alphabet : A, B
+    Constants : F + -
+    Axiom : A
+    Production rules:
+        A -> - B F + A F A + F B -
+        B -> + A F - B F B - F A +
+    '''
+
+    if order <= 0:
+        return
+
+    if 'A' == state:
+        turtle.left(90)
+        hilbert(turtle, 'B', order-1, side_length)
+        turtle.forward(side_length)
+        turtle.right(90)
+        hilbert(turtle, 'A', order-1, side_length)
+        turtle.forward(side_length)
+        hilbert(turtle, 'A', order-1, side_length)
+        turtle.right(90)
+        turtle.forward(side_length)
+        hilbert(turtle, 'B', order-1, side_length)
+        turtle.left(90)
+    elif 'B' == state:
+        turtle.right(90)
+        hilbert(turtle, 'A', order-1, side_length)
+        turtle.forward(side_length)
+        turtle.left(90)
+        hilbert(turtle, 'B', order-1, side_length)
+        turtle.forward(side_length)
+        hilbert(turtle, 'B', order-1, side_length)
+        turtle.left(90)
+        turtle.forward(side_length)
+        hilbert(turtle, 'A', order-1, side_length)
+        turtle.right(90)
+    else:
+        return "FAIL"
+
+def run_hilbert():
+    myTurtle = turtle.Turtle()
+    myScreen = turtle.Screen()
+    hilbert(turtle, 'A', 2, 60)
+    myScreen.exitonclick()
+
+def koch_side(turtle, order, side_length):
+    if 0 == order:
+        turtle.forward(side_length)
+    else:
+        koch(turtle, order-1, side_length/3)
+        turtle.left(60)
+        koch(turtle, order-1, side_length/3)
+        turtle.right(60)
+        turtle.right(60)
+        koch(turtle, order-1, side_length/3)
+        turtle.left(60)
+        koch(turtle, order-1, side_length/3)
+
+def run_snowflake():
+    myTurtle = turtle.Turtle()
+    myScreen = turtle.Screen()
+
+    koch_side(myTurtle, 2, 200)
+
+    myScreen.exitonclick()
+
+def problem14():
+    items = [ {'weight': 2, 'value': 3},
+              {'weight': 3, 'value': 4},
+              {'weight': 4, 'value': 8},
+              {'weight': 5, 'value': 8},
+              {'weight': 9, 'value': 10} ]
+
+    total_weight = 20
+    weight_to_value = [0]
+    for weight in range(1,total_weight+1):
+        prev_value = weight_to_value[weight-1]
+        choices = filter(lambda i: i['weight'] <= weight, items)
+
+        if [] == choices:
+            curr_value = prev_value
+        else:
+            curr_value = max(weight_to_value[weight-item['weight']] + item['value'] for item in choices)
+
+        weight_to_value.append(curr_value)
+
+    return max(weight_to_value)
+
+def problem14(items, total_weight):
+    '''
+    You've got `total_weight` pounds of weight that you can carry.
+    And you've got a list of items [{'weight', 'value'}]. You'd like
+    to maximize the value that you pack into the bag.
+    There are an infinite number of each item.
+    '''
+    weight_to_value = [0]
+    for weight in range(1,total_weight+1):
+        prev_value = weight_to_value[weight-1]
+        choices = filter(lambda i: i['weight'] <= weight, items)
+
+        if [] == choices:
+            curr_value = prev_value
+        else:
+            curr_value = max(weight_to_value[weight-item['weight']] + item['value'] for item in choices)
+
+        weight_to_value.append(curr_value)
+
+    return max(weight_to_value)
+
+items = [ {'weight': 2, 'value': 3},
+          {'weight': 3, 'value': 4},
+          {'weight': 4, 'value': 8},
+          {'weight': 5, 'value': 8},
+          {'weight': 9, 'value': 10} ]
+
+problem14(items, 20)
+
+def edit_distance_recursive(a, b):
+    if not a:
+        return len(b) * ins_cost
+    if not b:
+        return len(a) * ins_cost
+
+    return min(edit_distance(a[1:], b[1:]) + (a[0] != b[0]) * sub_cost,
+                edit_distance(a[1:], b) + sub_cost,
+                edit_distance(a, b[1:]) + del_cost)
+
+edit_distance_recursive("ab", "c")
